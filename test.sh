05054d5c11de79f3aaec0a5d88b329f23f6b0408
William Kapke <william.kapke@gmail.com>
2016-03-03 03:57:09 +0000

    test with & without --es_staging

--- test.sh
+++ test.sh
@@ -21,12 +21,14 @@ node extract.js > ./testers.json
 echo
 echo 'running the tests on each version of node...'
 while read v; do
+  n use $v test.js
   n use $v --es_staging test.js
 done < .versions
 
 
 LATEST=$(curl -sL https://nodejs.org/download/nightly/index.tab |   awk '{ if (!f && NR > 1) { print $1; f = 1 } }')
 PROJECT_NAME="node" PROJECT_URL="https://nodejs.org/download/nightly/" n project $LATEST
+node test.js
 node --es_staging test.js
 
 git add ./results/*.json
