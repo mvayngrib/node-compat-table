5d4ed452802e90ea5334e3c61e2eaa3386d235aa
Douglas Christopher Wilson <doug@somethingdoug.com>
2016-05-12 01:52:47 -0400

    run tests with callbacks and add node.js 0.10

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const testers = require('./testers.json');
 const fs = require('fs');
-var successful = 0;
 var version = process.versions.node;
 
 var es_staging = /--es_staging/.test(process.execArgv)? '--es_staging' : '';
@@ -38,31 +37,80 @@ global.__createIterableObject = function (arr, methods) {
 var results = {
   _version: version,
   _v8: process.versions.v8,
+  _completed: 0,
   _successful: 0,
-  _count: 0,
+  _count: Object.keys(testers).length,
   _percent: 0
 };
 
-function errors(e){ return !!e && e.message; }
+Object.keys(testers).forEach(function(name) {
+  var script = testers[name];
+  results[name] = false; //make SURE it makes it to the output
 
-Promise.all(
-  Object.keys(testers).map(function(name) {
-    var script = testers[name];
-    results[name] = false; //make SURE it makes it to the output
-    results._count++;
+  run(script, function(result) {
+    //expected results: `e.message` or true/false
+    results[name] = typeof result==='string'? result : !!result;
+    if(results[name]===true) results._successful++;
 
-    return run(script)
-    .catch(errors) //we don't want the `all` to catch any errors
-    .then(function(result) {
-      //expected results: `e.message` or true/false
-      results[name] = typeof result==='string'? result : !!result;
-      if(results[name]===true) successful++;
+    results._completed++;
+    if(results._completed===results._count) write();
+  });
+});
+
+function run(script, cb) {
+  //kangax's Promise tests reply on a asyncTestPassed function.
+  var async = /asyncTestPassed/.test(script);
+  if(async) {
+    runAsync(script, function(result) {
+      if(result===false) return runAsync(strict(script), cb);
+      return cb(result);
     });
-  })
-)
-.then(function () {
-  results._successful = successful;
-  results._percent = successful/results._count;
+  }
+  else {
+    var result = runSync(script);
+    if(result===false) result = runSync(strict(script));
+    return cb(result);
+  }
+}
+
+function runAsync(script, cb) {
+  var timer = null;
+
+  try {
+    var fn = new Function("asyncTestPassed", script);
+
+    fn(function() {
+      clearTimeout(timer);
+      process.nextTick(cb, true);
+    });
+
+    timer = setTimeout(function() {
+      cb(false);
+    }, 500);
+  }
+  catch(e){
+    clearTimeout(timer);
+    process.nextTick(cb, e.message);
+  }
+}
+
+function runSync(script) {
+  try {
+    var fn = new Function(script);
+    return fn() || false;
+  }
+  catch(e){
+    return e.message;
+  }
+}
+
+function strict(script) {
+  return '"use strict"\n' + script;
+}
+
+function write() {
+  delete results._completed;
+  results._percent = results._successful/results._count;
   var json = JSON.stringify(results, null, 2);
   if(/nightly/.test(version)) {
     version = 'nightly';
@@ -70,26 +118,4 @@ Promise.all(
     if(harmony) version+='--harmony';
   }
   fs.writeFileSync(__dirname+'/results/'+version+'.json', json);
-})
-.catch(console.log);
-
-function run(script) {
-  //kangax's Promise tests reply on a asyncTestPassed
-  //function that doesn't do anything that works here.
-  // So, we'll swap it out to resolve/reject a promise when done.
-  var async = /asyncTestPassed/.test(script);
-  if(async) script = script.replace(/asyncTestPassed\(\)/, 'accept(true)') + '\nsetTimeout(reject, 500);';
-
-  try {
-    var fn = new Function("accept, reject", script);
-    return (async? new Promise(fn) : Promise.resolve(fn() || Promise.reject()))
-      //if it fails, try using strict mode...
-      .catch(function() {
-        var fn = new Function("accept, reject", '"use strict"\n' + script);
-        return async? new Promise(fn) : Promise.resolve(fn());
-      });
-  }
-  catch(e){
-    return Promise.resolve(e.message);
-  }
 }
